af274205e386a8354c25b62b04ade87e29f15a6d
aixancura <curaaixan4@gmail.com>
2025-08-22 17:43:31 -0300

    agregando array de productos

---
+++ js/archivo.js
@@ -0,0 +1,99 @@
+const productos = [
+  {
+    id: 1,
+    name: "Silla de Trabajo Belgrano",
+    price: 25000,
+    image: "/img/Silla de Trabajo Belgrano.png",
+    alt: "Silla ergonómica de trabajo modelo Belgrano",
+    link: "/producto.html?id=1"
+  },
+  {
+    id: 2,
+    name: "Aparador Uspallata",
+    price: 48000,
+    image: "/img/Aparador Uspallata.png",
+    alt: "Aparador Uspallata",
+    link: "/producto.html?id=2"
+    
+  },
+  {
+    id: 3,
+    name: "Biblioteca Recoleta",
+    price: 35000,
+    image: "/img/Biblioteca Recoleta.png",
+    alt: "Biblioteca Recoleta",
+    link: "/producto.html?id=3"
+  },
+  {
+    id: 4,
+    name: "Butaca Mendoza",
+    price: 60000,
+    image: "/img/Butaca Mendoza.png",
+    alt: "Butaca Mendoza",
+    link: "/producto.html?id=4"
+  },
+  {
+    id: 5,
+    name: "Escritorio Costa",
+    price: 70000,
+    image: "/img/Escritorio Costa.png",
+    alt: "Escritorio Costa",
+    link: "/producto.html?id=5"
+  },
+  {
+    id: 6,
+    name: "Mesa Comedor Pampa",
+    price: 80000,
+    image: "/img/Mesa Comedor Pampa.png",
+    alt: "Mesa Comedor Pampa",
+    link: "/producto.html?id=6"
+  },
+  {
+    id: 7,
+    name: "Mesa de Centro Araucaria",
+    price: 80000,
+    image: "/img/Mesa de Centro Araucaria.png",
+    alt: "Mesa de Centro Araucaria",
+    link: "/producto.html?id=7"
+  },
+  {
+    id: 8,
+    name: "Mesa de Noche Aconcagua",
+    price: 40000,
+    image: "/img/Mesa de Noche Aconcagua.png",
+    alt: "Mesa de Noche Aconcagua",
+    link: "/producto.html?id=8"
+  },
+  {
+    id: 9,
+    name: "Silla de Trabajo Belgrano",
+    price: 250000,
+    image: "/img/Silla de Trabajo Belgrano.png",
+    alt: "Silla ergonómica de trabajo modelo Belgrano",
+    link: "/producto.html?id=9"
+  },
+  {
+    id: 10,
+    name: "Sillas Córdoba",
+    price: 30000,
+    image: "/img/Sillas Córdoba.png",
+    alt: "Sillas modelo Córdoba",
+    link: "/producto.html?id=10"
+  },
+  {
+    id: 11,
+    name: "Sillón Copacabana",
+    price: 120000,
+    image: "/img/Sillón Copacabana.png",
+    alt: "Sillón modelo Copacabana",
+    link: "/producto.html?id=11"
+  },
+  {
+    id: 12,
+    name: "Sofá Patagonia",
+    price: 150000,
+    image: "/img/Sofá Patagonia.png",
+    alt: "Sofá modelo Patagonia",
+    link: "/producto.html?id=12"
+  }
+];
