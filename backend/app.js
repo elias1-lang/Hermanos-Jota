fffba289b25bff8a7c6584c3067d62b8cd58bdfe
elenairurueta <84137564+elenairurueta@users.noreply.github.com>
2025-11-02 18:25:03 -0300

    mover img a /public

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Producto = require("./models/Producto");
 const Categoria = require("./models/Categoria");
 
@@ -7,41 +7,45 @@ const productoRouter = require("./routers/productoRouter");
 const categoriaRouter = require("./routers/categoriaRouter");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-const DB_URI = process.env.VARIABLE_DB_URI; 
-    //para conectarse a la BDD (de necesitarse), reemplazar toda la variable process.env.VARIABLE_DB_URI
-        //ANTES DE HACER PULL reemplazarla con "process.env.VARIABLE_DB_URI" con la original, tal que la variable secreta no se copia al repositorio publico
-            //para ejecutar sin reemplazar nada, es decir con: const DB_URI = process.env.VARIABLE_DB_URI;
-            //crear archivo ".env", crear una variable de forma: VARIABLE_DB_URI = "[text]"; 
-            //siendo [text] la url de mongodb atlas
-                //luego en terminal bash: node --env-file .env app.js
+const DB_URI = process.env.VARIABLE_DB_URI;
+//para conectarse a la BDD (de necesitarse), reemplazar toda la variable process.env.VARIABLE_DB_URI
+//ANTES DE HACER PULL reemplazarla con "process.env.VARIABLE_DB_URI" con la original, tal que la variable secreta no se copia al repositorio publico
+//para ejecutar sin reemplazar nada, es decir con: const DB_URI = process.env.VARIABLE_DB_URI;
+//crear archivo ".env", crear una variable de forma: VARIABLE_DB_URI = "[text]";
+//siendo [text] la url de mongodb atlas
+//luego en terminal bash: node --env-file .env app.js
 
-mongoose.connect(DB_URI)
-.then(()=>console.log("Conexión a la BDD"))
-.catch(err => {console.log("Error en la conexión:" + err); mongoose.disconnect();})
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Conexión a la BDD"))
+  .catch((err) => {
+    console.log("Error en la conexión:" + err);
+    mongoose.disconnect();
+  });
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); // Middleware para parsear datos del formulario
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("Bienvenido a hermanos jota");
+app.get("/", (req, res) => {
+  res.send("Bienvenido a hermanos jota");
 });
 
-app.use("/api/productos/",productoRouter);
-app.use("/api/categorias/",categoriaRouter);
+app.use("/api/productos/", productoRouter);
+app.use("/api/categorias/", categoriaRouter);
 
-
-app.use((req,res,next)=>{
-    res.status(404).send("Página no encontrada");
+app.use((req, res, next) => {
+  res.status(404).send("Página no encontrada");
 });
 
-
-app.use((err,req,res,next)=>{
-    console.error(err.stack);
-    res.status(500).send("Error interno del servidor");
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor");
 });
 
 app.listen(PORT, () => {
-    console.log("Servidor Escuchando el Puerto:" + PORT);
-    console.log("Acceso a: http://localhost:"+ PORT);
+  console.log("Servidor Escuchando el Puerto:" + PORT);
+  console.log("Acceso a: http://localhost:" + PORT);
 });
